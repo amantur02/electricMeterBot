e249fe61c6e553c447092a108060c6ce789e8d87
amantur02 <amanamanovica@gmail.com>
2024-01-03 21:18:37 +0600

    first commit

---
+++ resource_access/db_base.py
@@ -0,0 +1,2 @@
+from .db_base_class import Base
+from .db_models import *
---
+++ resource_access/db_models.py
@@ -0,0 +1,30 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean, Float, ForeignKey
+from .db_base_class import Base
+
+
+class Residents(Base):
+    __tablename__ = 'residents'
+
+    id = Column(Integer, primary_key=True)
+    home_number = Column(String)
+    name = Column(String)
+    phone_number = Column(String)
+    monthly_readings = Column(Date)
+    readings = Column(Integer)
+
+
+class ElectricityReading(Base):
+    __tablename__ = 'electricity_readings'
+
+    id = Column(Integer, primary_key=True)
+    resident_id = Column(
+        Integer,
+        ForeignKey("residents.id", ondelete="RESTRICT"),
+        nullable=False,
+    )
+    last_reading = Column(Integer, nullable=True)
+    current_reading = Column(Integer, nullable=False)
+    payment_status = Column(Boolean, default=False)
+    amount = Column(Float)
+    consumed_kwh = Column(Float)
+    created_date = Column(Date)
