38afc7214b9488e64b1697b33667fb3814cf2deb
amantur02 <amanamanovica@gmail.com>
2024-01-21 15:55:55 +0600

    update bot 2

--- forms.py
+++ forms.py
@@ -1,6 +1,13 @@
-from aiogram.fsm.state import StatesGroup, State
-
-
-class Resident(StatesGroup):
-    home_number = State()
-    name = State()
+# from aiogram.fsm.state import StatesGroup, State
+#
+#
+# class Resident(StatesGroup):
+#     home_number = State()
+#     name = State()
+#
+#
+# class ElectricityReading(StatesGroup):
+#     resident_id = State()
+#     current_reading = State()
+#     payment_status = State()
+#     amount = State()
--- resource_access/db_models.py
+++ resource_access/db_models.py
@@ -1,27 +1,24 @@
-from sqlalchemy import Column, Integer, String, Date, Boolean, Float, ForeignKey
+from sqlalchemy import Column, Integer, String, Date, Boolean, Float, ForeignKey, TIMESTAMP, func
 from .db_base_class import Base
 
 
 class ResidentDB(Base):
     __tablename__ = 'residents'
 
-    id = Column(Integer, primary_key=True)
     home_number = Column(String)
     name = Column(String)
     phone_number = Column(String)
 
 
-class ElectricityReading(Base):
+class ElectricityReadingDB(Base):
     __tablename__ = 'electricity_readings'
 
-    id = Column(Integer, primary_key=True)
-    resident_id = Column(
-        Integer,
-        ForeignKey("residents.id", ondelete="RESTRICT"),
-        nullable=False,
-    )
-    last_reading = Column(Integer, nullable=True)
+    # resident_id = Column(
+    #     Integer,
+    #     ForeignKey("residents.id", ondelete="RESTRICT"),
+    #     nullable=False,
+    # )
     current_reading = Column(Integer, nullable=False)
     payment_status = Column(Boolean, default=False)
-    amount = Column(Float)
-    created_date = Column(Date)
+    amount = Column(Float, nullable=True)
+    # created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
