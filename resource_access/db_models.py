069101cde11be7a4e7441c9e04b2c3613c1c3954
amantur02 <amanamanovica@gmail.com>
2024-01-22 16:01:00 +0600

    update db_models

--- resource_access/db_models.py
+++ resource_access/db_models.py
@@ -5,9 +5,9 @@ from .db_base_class import Base
 class ResidentDB(Base):
     __tablename__ = 'residents'
 
-    home_number = Column(String)
-    name = Column(String)
-    phone_number = Column(String)
+    home_number = Column(String, nullable=False)
+    name = Column(String, nullable=True)
+    phone_number = Column(String, nullable=True)
 
 
 class ElectricityReadingDB(Base):
@@ -18,7 +18,10 @@ class ElectricityReadingDB(Base):
         ForeignKey("residents.id", ondelete="RESTRICT"),
         nullable=False,
     )
-    current_reading = Column(Integer, nullable=False)
+    current_kwh = Column(Integer, nullable=False)
+    increased_kwh = Column(Integer, nullable=True)
     payment_status = Column(Boolean, default=False)
     amount = Column(Float, nullable=True)
+    increased_amount = Column(Float, nullable=True)
+    not_increased_amount = Column(Float, nullable=True)
     created_at = Column(TIMESTAMP(timezone=True), server_default=func.now())
